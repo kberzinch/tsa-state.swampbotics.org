d718384ae37eba04bee378c009454e0e771ac502
kberzinch <kberzinch@e7ae92a1-e9a4-4d3f-8a7a-ddb19a90cc43>
2015-03-15 03:09:31 +0000

    Data now prepended with data for some reason

--- webroot/data.php
+++ webroot/data.php
@@ -9,4 +9,4 @@ header('Content-Type: application/json');
 $db = new Db();
 $numqualifying = $db->query('SELECT value FROM settings');
 $teams = $db->query('SELECT * FROM scores ORDER BY (program_score+driver_score), program_score DESC LIMIT ?', 'i', $numqualifying[0]['value']);
-echo json_encode($teams);
+echo '{"data" : '.json_encode($teams).'}';
