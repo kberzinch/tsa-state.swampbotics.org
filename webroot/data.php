4a2c0bacc1dbcd72f98282b0d528d89a9c137e56
msultan <msultan@e7ae92a1-e9a4-4d3f-8a7a-ddb19a90cc43>
2015-03-15 07:18:09 +0000

    idk what I'm doing

--- webroot/data.php
+++ webroot/data.php
@@ -8,5 +8,4 @@ header('Content-Type: application/json');
 $db = new Db();
 $numqualifying = $db->query('SELECT value FROM settings');
 $teams = $db->query('SELECT * FROM scores ORDER BY (program_score+driver_score), program_score DESC LIMIT ?', 'i', $numqualifying[0]['value']);
-$teams = str_replace(null, '', $teams);
-echo json_encode($teams);
+echo json_encode(str_replace(null, '', $teams));
