4ea08984547739b680ec7eb552ab4854515d0b82
Kristaps Berzinch <kberzinch@gatech.edu>
2015-03-15 13:20:00 +0000

    data.php: nulls are replaced with empty strings

--- webroot/data.php
+++ webroot/data.php
@@ -8,4 +8,12 @@ header('Content-Type: application/json');
 $db = new Db();
 $numqualifying = $db->query('SELECT value FROM settings');
 $teams = $db->query('SELECT * FROM scores ORDER BY (program_score+driver_score), program_score DESC LIMIT ?', 'i', $numqualifying[0]['value']);
+for ($i = 0; $i < count($teams); $i++) {
+    if ($teams[$i]['program_score'] === null) {
+        $teams[$i]['program_score'] = '';
+    }
+    if ($teams[$i]['driver_score'] === null) {
+        $teams[$i]['driver_score'] = '';
+    }
+}
 echo json_encode($teams);
