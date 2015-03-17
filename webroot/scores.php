632574e9072bf9fe5a1b3745cb5dae14040da37e
Kristaps Berzinch <kberzinch@gatech.edu>
2015-03-17 16:14:27 +0000

    Fixed order of score data (DESC must be specified for each column?)

--- webroot/scores.php
+++ webroot/scores.php
@@ -5,7 +5,7 @@ require "../authenticate.php";
 require "../db.class.php";
 
 $db = new Db();
-$teams = $db->query('SELECT *, (program_score+driver_score) FROM scores ORDER BY (program_score+driver_score), program_score DESC');
+$teams = $db->query('SELECT *, (program_score+driver_score) FROM scores ORDER BY (program_score+driver_score) DESC, program_score DESC');
 $data = '{data}';
 for ($i = 0; $i < count($teams); $i++) {
     $data = str_replace('{data}', '<tr><td>'.($i + 1).'</td><td>'.$teams[$i]['vin'].'</td><td>'.$teams[$i]['program_skyrise_sections'].'</td><td>'.$teams[$i]['program_skyrise_cubes'].'</td><td>'.$teams[$i]['program_posts_owned'].'</td><td>'.$teams[$i]['program_posts_cubes'].'</td><td>'.$teams[$i]['program_floor_cubes'].'</td><td>'.$teams[$i]['program_score'].'</td><td>'.$teams[$i]['driver_skyrise_sections'].'</td><td>'.$teams[$i]['driver_skyrise_cubes'].'</td><td>'.$teams[$i]['driver_posts_owned'].'</td><td>'.$teams[$i]['driver_posts_cubes'].'</td><td>'.$teams[$i]['driver_floor_cubes'].'</td><td>'.$teams[$i]['driver_score'].'</td><td>'.$teams[$i]['(program_score+driver_score)'].'</td></tr>{data}', $data);
