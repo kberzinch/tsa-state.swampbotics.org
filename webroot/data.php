a8e2ae9d41a7f5fa5b9af0ba2cd497135e5af0f1
kberzinch <kberzinch@e7ae92a1-e9a4-4d3f-8a7a-ddb19a90cc43>
2015-03-17 16:51:59 +0000

    Scrubbed unneeded data from data.php

--- webroot/data.php
+++ webroot/data.php
@@ -7,7 +7,7 @@ header('Content-Type: application/json');
 
 $db = new Db();
 $numqualifying = $db->query('SELECT value FROM settings WHERE setting=0');
-$teams = $db->query('SELECT * FROM scores ORDER BY (program_score+driver_score) DESC, program_score DESC LIMIT ?', 'i', $numqualifying[0]['value']);
+$teams = $db->query('SELECT vin, program_score, driver_score FROM scores ORDER BY (program_score+driver_score) DESC, program_score DESC LIMIT ?', 'i', $numqualifying[0]['value']);
 for ($i = 0; $i < count($teams); $i++) {
     if ($teams[$i]['program_score'] === null) {
         $teams[$i]['program_score'] = '';
