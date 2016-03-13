bf0f7855e46d6a0f8cf6e844fb98a64a041832ac
Berzinch, Kristaps T <kberzinch@gatech.edu>
2016-03-12 20:24:23 -0600

    Fixes #3
    Maybe

---
+++ webroot/scores.csv.php
@@ -0,0 +1,17 @@
+<?php
+namespace TSAState;
+
+require_once "../php/authenticate.php";
+require_once "../php/db.class.php";
+
+header('Content-type: text/csv');
+header('Content-disposition: attachment;filename=scores.csv');
+
+$db = new Db();
+$teams = $db->query('SELECT *, (program_score+driver_score) FROM scores ORDER BY (program_score+driver_score) DESC, program_score DESC');
+$output = 'vin,pgl,pgh,pol,poh,pscore,dgl,dgh,dol,doh,dscore,total';
+for ($i = 0; $i < count($teams); $i++) {
+    $output .= $teams[$i]['vin'].','.$teams[$i]['program_balls_low'].','.$teams[$i]['program_balls_high'].','.$teams[$i]['program_bonus_low'].','.$teams[$i]['program_bonus_high'].','.$teams[$i]['program_score'].','.$teams[$i]['driver_balls_low'].','.$teams[$i]['driver_balls_high'].','.$teams[$i]['driver_bonus_low'].','.$teams[$i]['driver_bonus_high'].','.$teams[$i]['driver_score'].','.$teams[$i]['(program_score+driver_score)'];
+}
+
+echo $output;
